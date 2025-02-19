b9c3a3a6395e891cfceb11b0b94521629465c726
kunalsoni20 <161117242+kunalsoni20@users.noreply.github.com>
2025-02-19 20:51:16 +0530

    routes.py

---
+++ routes.py
@@ -0,0 +1,31 @@
+from flask import render_template, request, redirect, url_for
+from app import app, db
+from models import Todo
+
+@app.route('/')
+def index():
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    content = request.form.get('content')
+    if content:
+        new_todo = Todo(content=content)
+        db.session.add(new_todo)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:id>')
+def complete(id):
+    todo = Todo.query.get_or_404(id)
+    todo.completed = not todo.completed
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    todo = Todo.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
